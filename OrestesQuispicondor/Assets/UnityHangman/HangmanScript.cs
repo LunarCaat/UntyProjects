6b937a07dbe0a1f4e5107fa7f01f48e83fc692bb
setseroZZ <resetos35@gmail.com>
2018-03-27 20:57:24 -0500

    Avance hasta antes del break

---
+++ OrestesQuispicondor/Assets/UnityHangman/HangmanScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HangmanScript : MonoBehaviour {
+    public string palabra;
+    string palabraEscondida;
+    public Text output; 
+
+    // Use this for initialization
+    void Start() { 
+        
+        foreach (char c in palabra)
+        {
+            palabraEscondida += "*";
+        }
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        output.text = palabraEscondida;
+        //Debug.Log(palabra);
+
+    }
+}
