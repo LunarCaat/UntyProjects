dbc0c6ae6e4e15ec8d9424d846ac766a3ef45d9c
setseroZZ <resetos35@gmail.com>
2018-04-03 20:53:19 -0500

    El loop del juego esta en un metodo. Ademas tiene nuevos sonidos.

--- OrestesQuispicondor/Assets/UnityHangman/HangmanScript.cs
+++ OrestesQuispicondor/Assets/UnityHangman/HangmanScript.cs
@@ -8,33 +8,44 @@ public class HangmanScript : MonoBehaviour {
     string palabraEscondida;
     public Text outputText;
     public InputField inputText;
+    public int maximoFallas = 4;
 
     public AudioClip successSound;
     public AudioClip failSound;
+    public AudioClip winSound;
     public AudioSource camSource;
 
+    private bool endHangman=false;
+
     // Use this for initialization
-    void Start() { 
-        
+    void Start() {
+        inputText.Select();
         foreach (char c in palabra)
         {
             palabraEscondida += "*";
         }
     }
-    
+    void Update() {
+        if (!endHangman)
+        {
+            RunHangman();
+        }
+
+    }
 	// Update is called once per frame
-	void Update () {
+	void RunHangman () {
         if (outputText.text != palabraEscondida)
         {
             outputText.text = palabraEscondida;
         }
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && !string.IsNullOrEmpty(inputText.text))
         {
-            Debug.Log("Enter pressed!");
+             Debug.Log("Enter pressed!");
             //char letra = inputText.text[0];
             string letra = inputText.text.Substring(0, 1);
             if (palabra.Contains(letra))
             {
+                //Acerto
                 string palabraTemporal = "";
                 for (int i = 0; i < palabra.Length; i++)
                 {
@@ -53,15 +64,25 @@ public class HangmanScript : MonoBehaviour {
             }
             else
             {
+                //Fallo
                 camSource.PlayOneShot(failSound);
             }
             inputText.text = "";
+            inputText.Select();
+            if (palabraEscondida == palabra)
+            {
+                outputText.text = "FELICIDADES, GANASTE!";
+                outputText.fontStyle = FontStyle.Bold;
+                inputText.gameObject.SetActive(false);
+                endHangman = true;
+                Debug.Log("Felicidades");
+                camSource.PlayOneShot(winSound);
+            }
+
         }
+        inputText.ActivateInputField();
 
         //Debug.Log(palabra);
-        if (palabraEscondida== palabra)
-        {
-            Debug.Log("Felicidades");
-        }
+       
     }
 }
