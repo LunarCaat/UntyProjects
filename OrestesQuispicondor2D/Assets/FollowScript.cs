145c3cfdfa31df322d323f0b5206cb5c965802b2
setseroZZ <resetos35@gmail.com>
2018-05-15 20:29:59 -0500

    1erAvance8yMedia

--- OrestesQuispicondor2D/Assets/FollowScript.cs
+++ OrestesQuispicondor2D/Assets/FollowScript.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class FollowScript : MonoBehaviour {
     private Transform target;
+    public float speed = 1;
+    public TriggerArea trigger;
     // Use this for initialization
     void Start () {
 		//PorHacer Inicializar
+		target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(trigger==null||(trigger!=null&&trigger.hasActivated))
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 	}
 }
---
+++ OrestesQuispicondor2D/Assets/TriggerArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour {
+	public bool hasActivated=false;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            hasActivated=true;
+        }
+
+        
+    }
+}
