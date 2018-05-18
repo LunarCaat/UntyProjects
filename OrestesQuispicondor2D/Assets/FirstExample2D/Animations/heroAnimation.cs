0b577dbd283b21da87c83922b7bbb61c0f113124
setseroZZ <resetos35@gmail.com>
2018-05-17 21:12:31 -0500

    Fin del break

---
+++ OrestesQuispicondor2D/Assets/FirstExample2D/Animations/heroAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroAnimation : MonoBehaviour {
+
+
+
+	private Animator animator;
+	//public RunAnimatorController animation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
