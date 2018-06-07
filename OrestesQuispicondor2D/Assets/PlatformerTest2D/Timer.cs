ee1ea46f08d7863c3d3cc30a867a425145ef0a47
setseroZZ <resetos35@gmail.com>
2018-06-07 15:38:37 -0500

    Timer con formato.

--- OrestesQuispicondor2D/Assets/PlatformerTest2D/Timer.cs
+++ OrestesQuispicondor2D/Assets/PlatformerTest2D/Timer.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
-    float timeLeft;
-    public float timeStart;
+    public float minutesLeft;
+    public float secondsLeft;
+    //public float timeStart;
+    private float timeLeft;
+    private bool stopped=false;
     public Text timerText;
 
-	// Use this for initialization
-	void Start () {
-        timeLeft = timeStart;
+    // Use this for initialization
+    void Start() {
+        timeLeft = minutesLeft*60 + secondsLeft;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (!stopped)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < float.Epsilon)
+            {
+                StopClock();
+                timerText.text = "Time:00:00";
+                TimeRanOut();
+            }
+            else
+            {
+                string minutes = Mathf.Floor(timeLeft / 60).ToString("00");
+                string seconds = Mathf.Floor(timeLeft % 60).ToString("00");
+                timerText.text = "Time:" + minutes + ":" + seconds;
+            }
+                
+            
+        }
+        
+
+
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        timeLeft -= Time.deltaTime;
-        timerText.text = "Time:"+timeLeft.ToString("0.00");
-	}
+    private void StopClock()
+    {
+        
+        stopped = true;
+    }
+    private void TimeRanOut()
+    {
+        
+    }
 }
