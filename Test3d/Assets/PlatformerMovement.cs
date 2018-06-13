dd70eeda1cd8d89f8b6f1ee22a412151fbac6b8f
setseroZZ <resetos35@gmail.com>
2018-06-13 11:12:10 -0500

    Ir averiguando lo de la camara y transformaciones de matrices

--- Test3d/Assets/PlatformerMovement.cs
+++ Test3d/Assets/PlatformerMovement.cs
@@ -10,29 +10,29 @@ public class PlatformerMovement : MonoBehaviour {
     public Rigidbody rigidbody3D;
     public float impulseValue;
     public bool isGrounded;
-    public float rayDetectionDistance = 0.3f;
-    Vector3 leftNode { get { return transform.position - new Vector3(0.3f, 1, 0); } }
-    Vector3 rightNode { get { return transform.position + new Vector3(0.3f, -1, 0); } }
+    public float rayDetectionDistance = 0.15f;
+    Vector3 leftNode { get { return transform.position - new Vector3(0.3f, 0.9f, 0); } }
+    Vector3 rightNode { get { return transform.position + new Vector3(0.3f, -0.9f, 0); } }
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         bool downLeft = Physics.Raycast(leftNode, Vector3.down, rayDetectionDistance);
         bool downRight = Physics.Raycast(rightNode, Vector3.down, rayDetectionDistance);
         movement = transform.position;
         float horizontalDirection = Input.GetAxis("Horizontal");
         float verticalDirection = Input.GetAxis("Vertical");
 
-        if(Input.GetKey(KeyCode.J)){
+        if (Input.GetKey(KeyCode.J)) {
             transform.Rotate(Vector3.up * 100f * Time.deltaTime);
         }
 
 
-        if(horizontalDirection!=0||verticalDirection!=0){
-            movement += (Vector3.forward*verticalDirection+Vector3.right*horizontalDirection)*horizontalSpeed*Time.deltaTime;
+        if (horizontalDirection != 0 || verticalDirection != 0) {
+            movement += (Vector3.forward * verticalDirection + Vector3.right * horizontalDirection) * horizontalSpeed * Time.deltaTime;
         }
 
         if (isGrounded)
@@ -40,11 +40,11 @@ public class PlatformerMovement : MonoBehaviour {
             Debug.Log("It's Grounded");
             if (!downLeft && !downRight)
             {
-                
+
                 isGrounded = false;
             }
             else if (Input.GetKeyDown(KeyCode.H))
-                
+
             {
                 Debug.Log("Jump!");
                 //verticalSpeed = impulseValue;
@@ -54,7 +54,7 @@ public class PlatformerMovement : MonoBehaviour {
         }
         else
         {
-            Debug.Log("Else");
+            Debug.Log("downLeft = " + downLeft.ToString() + " downRight= " + downRight.ToString());
             if (downLeft || downRight)
             {
                 Debug.Log("Raycast is Hit");
