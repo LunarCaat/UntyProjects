729d607aee9506a75c977bbf288fbb6e49d6d4cf
setseroZZ <resetos35@gmail.com>
2018-06-28 22:00:49 -0500

    Fin de clase

---
+++ Test3d/Assets/PlatformerTest2D/Script/ActivateShadowOnRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateShadowOnRenderer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        GetComponent<Renderer>().receiveShadows = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Test3d/Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public Image hpBar;
+    public Gradient barColors;
+    float hp;
+    public PlayerScript playerScript;
+
+	// Use this for initialization
+	void Start () {
+        hpBar.fillAmount = playerScript.normalizedHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(hpBar.fillAmount!=playerScript.normalizedHP){
+            float delta = Mathf.Abs(hpBar.fillAmount - playerScript.normalizedHP);
+            if (delta < 0.2f) { delta = 0.2f; }
+            hpBar.fillAmount = Mathf.MoveTowards(hpBar.fillAmount,playerScript.normalizedHP,delta * Time.deltaTime);
+            hpBar.color = barColors.Evaluate(hpBar.fillAmount);
+        }
+	}
+}
