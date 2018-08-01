6957d2bbb52e0bd0d104a4b742486ba64391a54f
setseroZZ <resetos35@gmail.com>
2018-08-01 16:09:51 -0500

    Antes de hacer lo del CharacterEntity y PlayerEntity

---
+++ ProjectMonster/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float maxHP = 1f;
+    public float currentHP;
+    //public PowerBallBehaviour currentPower;
+    bool sightMode;
+    public bool isSightMode { get { return sightMode; }}
+
+    public float normalizedHP { get { return currentHP / maxHP; } }
+
+
+    public void ModifyHP (float addValue) {
+        currentHP = Mathf.Clamp (currentHP + addValue, 0, maxHP);
+    }
+	
+    public void SetSightMode (bool status) {
+        sightMode = status;
+    }
+}
