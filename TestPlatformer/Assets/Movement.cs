5c441278d6db802dc05718fc5ac0916d9df2a364
setseroZZ <resetos35@gmail.com>
2018-08-09 10:01:59 -0500

    Prototipo con ground check y flip de sprite cuando voltea a la derecha o izquierda

--- TestPlatformer/Assets/Movement.cs
+++ TestPlatformer/Assets/Movement.cs
@@ -8,11 +8,22 @@ public class Movement : MonoBehaviour {
     public float jumpSpeed=10;
     public float health = 10;
     public float maxHealth = 10;
+	
+	
+	
+    public float rayDetectionDistance = 0.1f;
     public float normalizedHP { get { return health / maxHealth; } }
-
+    Vector3 leftNode { get { return transform.position - new Vector3 (0.5f, 1, 0); } }
+    Vector3 rightNode { get { return transform.position + new Vector3 (0.5f, -1, 0); } }
+	bool isGrounded;
+    SpriteRenderer spriteRenderer;
+	
+	
+	public bool isFlipped=false;
 	// Use this for initialization
 	void Start () {
 		
+        spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -22,20 +33,42 @@ public class Movement : MonoBehaviour {
         if(horizontalSpeed!=0){
             charRigidbody2D.velocity= new Vector2(horizontalSpeed* characterSpeed,charRigidbody2D.velocity.y);
         }
-
+		if (horizontalSpeed < 0) {
+            if (!spriteRenderer.flipX) { spriteRenderer.flipX = !isFlipped; }
+        } else if (horizontalSpeed > 0) {
+            if (spriteRenderer.flipX) { spriteRenderer.flipX = isFlipped; }
+        }
 
 	}
 
 	private void FixedUpdate()
 	{
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            charRigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-
+		
+        RaycastHit2D downLeft = Physics2D.Raycast (leftNode, Vector3.down, rayDetectionDistance);
+        RaycastHit2D downRight = Physics2D.Raycast (rightNode, Vector3.down, rayDetectionDistance);
+		
+		  if (isGrounded) {
+            if (!downLeft && !downRight) {
+                isGrounded = false;
+            } else if (Input.GetKeyDown (KeyCode.Space)) {
+                charRigidbody2D.AddForce (Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+                isGrounded = false;
+            }
+        } else {
+            if (downLeft || downRight) {
+                isGrounded = true;
+            }
         }
 	}
 
     public void TakeDamage(int damage){
         health -= damage;
+    }
+	
+	 void OnDrawGizmos () {
+        Gizmos.DrawSphere (leftNode, 0.2f);
+        Gizmos.DrawSphere (rightNode, 0.2f);
+        Gizmos.color = Color.white;
+        Gizmos.DrawRay (leftNode, Vector3.down * rayDetectionDistance);
     }
 }
